28a6c294c19dd92bdfd6615e2b7df78b382c4e45
loicyoane49-debug <loicyoane49@gmail.com>
2026-07-26 16:59:48 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-  apiKey: "VOTRE_API_KEY",
-  authDomain: "votre-projet.firebaseapp.com",
-  projectId: "votre-projet-id",
-  storageBucket: "votre-projet.appspot.com",
-  messagingSenderId: "VOTRE_MESSAGING_SENDER_ID",
-  appId: "VOTRE_APP_ID"
+  apiKey: "AIzaSyBDfAoS88t1nzD9xtz35dZJcnjekel50OI",
+  authDomain: "destie-shop.firebaseapp.com",
+  projectId: "destie-shop",
+  storageBucket: "destie-shop.firebasestorage.app",
+  messagingSenderId: "987910188705",
+  appId: "1:987910188705:web:95a17c66bb8ab5b7e24ae0"
 };
+
 
 firebase.initializeApp(firebaseConfig);
 
